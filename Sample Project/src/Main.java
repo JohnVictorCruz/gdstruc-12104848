ad4a642d08bc74aeab10994b6f68d410551f01db
John Victor Cruz <johnvictor.cruz@benilde.edu.ph>
2022-09-15 17:48:20 +0800

    Added Sample Project

--- Sample Project/src/Main.java
+++ Sample Project/src/Main.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) { //Short cut (psvm)
+        //prints out Hello World
+        //System.out.println("Hello World"); //Short cut (sout)
+
+
+        //get user input
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("What is your name?");
+        String name = scanner.nextLine();
+
+        System.out.println("Hello " + name);
+
     }
 }
